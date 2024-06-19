7fcd8d0f22e3e39b9d73f13d353bdf3fefee6cb7
Igor Kalchenko <igorkalchenko1997@gmail.com>
2024-06-19 17:47:52 +0300

    Add day 4

---
+++ Day 4. Rock, Paper, Scissors/Day 4. Rock, Paper, Scissors.py
@@ -0,0 +1,35 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+#Game_list = [rock, paper, scissors]
+#your_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors.\n"))
+#choice = Game_list[your_choice]
+#print(choice)
+import random
+a = random.randint(1, 2)
+print(a)
+#computer_choice = Game_list[random.randint(0, 2)]
+#print("Computer choice is\n" + computer_choice)
