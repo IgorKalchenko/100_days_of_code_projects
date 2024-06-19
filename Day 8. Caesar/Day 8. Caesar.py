edba5a5a3ba72474416414e99b63a01cfe6c593a
Igor Kalchenko <igorkalchenko1997@gmail.com>
2024-06-19 17:49:30 +0300

    Add day 8

---
+++ Day 8. Caesar/Day 8. Caesar.py
@@ -0,0 +1,48 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+print(logo)
+
+import math
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+decision = True
+def caesar(start_text, shift_amount, cipher_direction):
+    new_text = ""
+    for i in start_text:
+        if i in alphabet:
+            x = int(alphabet.index(i))
+            if cipher_direction == "encode":
+                new_position = x + shift_amount
+                new_num = new_position - 26 * math.floor(new_position / 26)
+            elif cipher_direction == "decode":
+                new_position = x - shift_amount
+                new_num = new_position + -26 * math.floor(new_position / 26)
+            new_text += alphabet[new_num]
+        else:
+            new_text += i
+    print(f"The {cipher_direction}d text is {new_text}")
+
+while decision:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    willing = input("Type 'yes' if you want to go again. Otherwise type 'no'\n")
+    if willing == "no":
+        decision = False
+        print("Goodbye")
